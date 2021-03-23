12dda66550d82098fd95783d8641babac406658a
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-03-23 20:35:03 +0430

    Jalaseh 4

---
+++ Jalaseh 4/BinarySearchTree.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include "BinarySearchTree.hpp"
+BinarySearchTree::BinarySearchTree(void)
+{
+    root = NULL;
+}
+BinarySearchTree::BinarySearchTree(const BinarySearchTree &Copy)
+{
+    root = NULL;
+    copy(Copy.root);
+}
+void BinarySearchTree::copy(BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    {
+        insert(node->getData());
+        copy(node->getLeft());
+        copy(node->getRight());
+    }
+}
+BinarySearchTree::~BinarySearchTree(void)
+{
+    root = deleteAllNodes(root);
+}
+void BinarySearchTree::inOrderTraverse(BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    inOrderTraverse(node->getLeft());
+    std::cout << node->getData() << " ";
+    inOrderTraverse(node->getRight());
+}
+void BinarySearchTree::inOrder(void)
+{
+    inOrderTraverse(root);
+}
+void BinarySearchTree::preOrderTraverse(BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    std::cout << node->getData() << " ";
+    preOrderTraverse(node->getLeft());
+    preOrderTraverse(node->getRight());
+}
+void BinarySearchTree::preOrder(void)
+{
+    preOrderTraverse(root);
+}
+void BinarySearchTree::postOrderTraverse(BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    postOrderTraverse(node->getLeft());
+    postOrderTraverse(node->getRight());
+    std::cout << node->getData() << " ";
+}
+void BinarySearchTree::postOrder(void)
+{
+    postOrderTraverse(root);
+}
+BinarySearchTree::Node *BinarySearchTree::insert(int data, BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        node = new BinarySearchTree::Node(data);
+    }
+    else
+    {
+        if (data > node->getData())
+        {
+            node->setRight(insert(data, node->getRight()));
+        }
+        else
+        {
+            node->setLeft(insert(data, node->getLeft()));
+        }
+    }
+    return node;
+}
+void BinarySearchTree::insert(int data)
+{
+    root = insert(data, root);
+}
+BinarySearchTree::Node *BinarySearchTree::remove(int data, BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    else if (data < node->getData())
+    {
+        node->setLeft(remove(data, node->getLeft()));
+    }
+    else if (data > node->getData())
+    {
+        node->setRight(remove(data, node->getRight()));
+    }
+    else if (node->getLeft() && node->getRight())
+    {
+        Node *temp;
+        temp = node->getRight();
+        while (temp->getLeft() != NULL)
+        {
+            temp = temp->getLeft();
+        }
+        node->setData(temp->getData());
+        node->setRight(remove(node->getData(), node->getRight()));
+    }
+    else
+    {
+        Node *temp;
+        temp = node;
+        if (node->getLeft() == NULL)
+        {
+            node = node->getRight();
+        }
+        else if (node->getRight() == NULL)
+        {
+            node = node->getLeft();
+        }
+        delete temp;
+    }
+    return node;
+}
+void BinarySearchTree::remove(int data)
+{
+    root = remove(data, root);
+}
+BinarySearchTree::Node *BinarySearchTree::deleteAllNodes(BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    {
+        deleteAllNodes(node->getLeft());
+        deleteAllNodes(node->getRight());
+        delete node;
+    }
+    return NULL;
+}
+BinarySearchTree::Node *BinarySearchTree::search(int data, BinarySearchTree::Node *node)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    else if (data < node->getData())
+    {
+        return search(data, node->getLeft());
+    }
+    else if (data > node->getData())
+    {
+        return search(data, node->getRight());
+    }
+    else
+    {
+        return node;
+    }
+}
+BinarySearchTree::Node *BinarySearchTree::search(int data)
+{
+    return search(data, root);
+}
+BinarySearchTree::Node::Node(int d)
+{
+    data = d;
+    left = NULL;
+    right = NULL;
+}
+BinarySearchTree::Node *BinarySearchTree::Node::getRight(void)
+{
+    return right;
+}
+BinarySearchTree::Node *BinarySearchTree::Node::getLeft(void)
+{
+    return left;
+}
+int BinarySearchTree::Node::getData(void)
+{
+    return data;
+}
+void BinarySearchTree::Node::setLeft(BinarySearchTree::Node *node)
+{
+    left = node;
+}
+void BinarySearchTree::Node::setRight(BinarySearchTree::Node *node)
+{
+    right = node;
+}
+void BinarySearchTree::Node::setData(int d)
+{
+    data = d;
+}
---
+++ Jalaseh 4/main.cpp
@@ -0,0 +1,31 @@
+/*
+--------------------------------------------------
+Name    : Binary Search Tree
+Author  : Danial Khorasanizadeh
+Course  : Advanced Programming - Spring 1400
+Date    : 23/Mar/2021
+--------------------------------------------------
+*/
+#include <iostream>
+#include "BinarySearchTree.hpp"
+int main(void)
+{
+    BinarySearchTree a;
+    a.insert(5);
+    a.insert(2);
+    a.insert(3);
+    a.insert(1);
+    a.insert(7);
+    a.insert(12);
+    a.insert(20);
+    a.insert(32);
+    a.insert(6);
+    a.insert(9);
+    BinarySearchTree::Node *b = a.search(12);
+    std::cout << b->getRight()->getData() << std::endl;
+    a.remove(12);
+    a.inOrder();
+    BinarySearchTree c = a;
+    c.postOrder();
+    return 0;
+}
